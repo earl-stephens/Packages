58ad67bf541b22977288ccec2ca4d68c84670238
earl-stephens <stephensearl@hotmail.com>
2022-01-28 23:06:42 -0700

    Provide notes on packages.

---
+++ src/application/App.java
@@ -0,0 +1,19 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		/* Packages are like folders, and do map to folders.
+		 * They are heirarchical.  You can also have sub packages.
+		 * Sub packages use a . to indicate the sub package.
+		 * The bin and src folders mirror each other.
+		 */
+
+		/* com.caveofprogramming.swingtest is a common type
+		 * of package name for a website code.  Need unique
+		 * package names, so put the com in front of the 
+		 * website URL.
+		 */
+	}
+
+}
